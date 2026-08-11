6fdf2f1ac9ae1b2bb569f652d1ee67cd5df55ea1
jalel ben sassi <jalelbensassi774@gmail.com>
2026-08-11 21:59:47 +0200

    test cicd pipeline

--- stb-frontend/stb-frontend/src/services/api.js
+++ stb-frontend/stb-frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL ='window.__ENV__?.API_URL';
+const API_BASE_URL = window.__ENV__?.API_URL || 'http://localhost:8080';
 
-/*window.__ENV__?.API_URL |*/
 
 const api = axios.create({
   baseURL: API_BASE_URL,
